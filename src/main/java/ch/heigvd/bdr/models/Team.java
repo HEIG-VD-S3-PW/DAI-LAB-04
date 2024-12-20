9c5f916900b40262c1807aea09a382706b4c861a
mathieuemery <mathieu.emery@heig-vd.ch>
2024-12-20 10:45:41 +0100

    Remove the list in teams

--- src/main/java/ch/heigvd/bdr/models/Team.java
+++ src/main/java/ch/heigvd/bdr/models/Team.java
@@ -6,7 +6,6 @@ public class Team {
   private int id;
   private String name;
   private int managerId;
-  private ArrayList<User> users = new ArrayList<User>();
 
   public Team(int id, String name, Integer managerId) {
     this.id = id;
@@ -39,12 +38,7 @@ public class Team {
   public int getManagerId() {
     return managerId;
   }
-
-  // TODO : Ask others if the user who has the id must be a manager
+  
   public void setManagerId(int managerId) {this.managerId = managerId;}
 
-  // So the list isn't editable
-  public ArrayList<User> getUsers() {return new ArrayList<>(users);}
-
-  public void addUser(User user) {this.users.add(user);}
 }
