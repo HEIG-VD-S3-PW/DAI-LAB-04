3ca64e819d7d0adafcbc8c2e77d3a9d3760ee9d3
mathieuemery <mathieu.emery@heig-vd.ch>
2024-12-21 17:39:03 +0100

    Methods for the UserDAO

--- src/main/java/ch/heigvd/bdr/dao/UserDAO.java
+++ src/main/java/ch/heigvd/bdr/dao/UserDAO.java
@@ -51,6 +51,27 @@ public class UserDAO implements GenericDAO<User, Integer> {
     }
   }
 
+  public User findByEmail(String email) throws ClassNotFoundException, SQLException, IOException {
+    String query = "SELECT * FROM \"User\" WHERE email = ?";
+    try (Connection conn = DatabaseUtil.getConnection();
+         PreparedStatement pstmt = conn.prepareStatement(query)) {
+      pstmt.setString(1, email);
+
+      try (ResultSet rs = pstmt.executeQuery()) {
+        if (rs.next()) {
+          User user = new User();
+          user.setId(rs.getInt("id"));
+          user.setFirstname(rs.getString("firstname"));
+          user.setLastname(rs.getString("lastname"));
+          user.setEmail(rs.getString("email"));
+          user.setRole(UserRole.valueOf(rs.getString("role")));
+          return user;
+        }
+      }
+      return null;
+    }
+  }
+
   @Override
   public List<User> findAll() throws ClassNotFoundException, SQLException, IOException {
     List<User> users = new ArrayList<>();
@@ -120,24 +141,86 @@ public class UserDAO implements GenericDAO<User, Integer> {
     }
   }
 
-  public User findByEmail(String email) throws ClassNotFoundException, SQLException, IOException {
-    String query = "SELECT * FROM \"User\" WHERE email = ?";
+  public boolean joinTeam(int userId, int teamId) throws ClassNotFoundException, SQLException, IOException {
+    String query = "INSERT INTO \"User_Team\" (userId, teamId) VALUES (?, ?)";
     try (Connection conn = DatabaseUtil.getConnection();
-        PreparedStatement pstmt = conn.prepareStatement(query)) {
-      pstmt.setString(1, email);
+         PreparedStatement pstmt = conn.prepareStatement(query)) {
+      pstmt.setInt(1, userId);
+      pstmt.setInt(2, teamId);
 
       try (ResultSet rs = pstmt.executeQuery()) {
         if (rs.next()) {
-          User user = new User();
-          user.setId(rs.getInt("id"));
-          user.setFirstname(rs.getString("firstname"));
-          user.setLastname(rs.getString("lastname"));
-          user.setEmail(rs.getString("email"));
-          user.setRole(UserRole.valueOf(rs.getString("role")));
-          return user;
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+
+  public Team createTeam(String name, int managerId) throws ClassNotFoundException, SQLException, IOException {
+    String query = "INSERT INTO \"Team\" (name, managerId) VALUES (?, ?) RETURNING id";
+    try (Connection conn = DatabaseUtil.getConnection();
+         PreparedStatement pstmt = conn.prepareStatement(query)) {
+      pstmt.setString(1, name);
+      pstmt.setInt(2, managerId);
+
+      try (ResultSet rs = pstmt.executeQuery()) {
+        if (rs.next()) {
+          Team team = new Team();
+          team.setId(rs.getInt("id"));
+          team.setName(name);
+          team.setManagerId(managerId);
+          return team;
         }
       }
       return null;
     }
   }
+
+  public Project createProject(String name) throws ClassNotFoundException, SQLException, IOException {
+    String query = "INSERT INTO \"Project\" (name) VALUES (?) RETURNING id";
+    try (Connection conn = DatabaseUtil.getConnection();
+         PreparedStatement pstmt = conn.prepareStatement(query)) {
+      pstmt.setString(1, name);
+
+      try (ResultSet rs = pstmt.executeQuery()) {
+        if (rs.next()) {
+          Project project = new Project();
+          project.setId(rs.getInt("id"));
+          project.setName(name);
+          return project;
+        }
+      }
+      return null;
+    }
+  }
+
+  public Goal createGoal(String name, String description, String note, String tag, int projectId, int teamId) throws ClassNotFoundException, SQLException, IOException {
+    String query = "INSERT INTO \"Goal\" (name, description, note, tag, projectId, teamId) VALUES (?, ?, ?, ?, ?, ?) RETURNING id";
+    try (Connection conn = DatabaseUtil.getConnection();
+         PreparedStatement pstmt = conn.prepareStatement(query)) {
+      pstmt.setString(1, name);
+      pstmt.setString(2, description);
+      pstmt.setString(3, note);
+      pstmt.setString(4, tag);
+      pstmt.setInt(5, projectId);
+      pstmt.setInt(6, teamId);
+
+      try (ResultSet rs = pstmt.executeQuery()) {
+        if (rs.next()) {
+          Goal goal = new Goal();
+          goal.setId(rs.getInt("id"));
+          goal.setName(name);
+          goal.setDescription(description);
+          goal.setNote(note);
+          goal.setTag(tag);
+          goal.setProjectId(projectId);
+          goal.setTeamId(teamId);
+          return goal;
+        }
+      }
+      return null;
+    }
+  }
+
 }
